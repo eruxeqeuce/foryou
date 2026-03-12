5cad4a540e906b5631895b4d668b37f1333446f9
eruxeqeuce <erujunangelo@gmail.com>
2026-03-12 19:51:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "okay, bye. ";
